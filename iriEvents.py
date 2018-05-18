d0168ba88ae0ff941b58a9b704f8331727c33d0a
Preston <childrep@vt.edu>
2018-05-17 20:24:42 -0400

    tweaked iri events script

--- iriEvents.py
+++ iriEvents.py
@@ -7,13 +7,24 @@ YEAR = 2018
 
 baseList = pd.read_csv('iriIN.csv')
 
-#File to save out to, will overwrite if script is rerun.
+#only count regionals, districts, dcmp, and worlds
+normalEvents = [0,1,2,3,5]
+
+#ignore michigan and ontario finals to avoid overcounting
+ignoreEvents = ['2018micmp', '2018oncmp']
+
 f = open("iriEvents" + str(YEAR) + ".csv", 'w', encoding='utf-8')
 f.write("team, events\n")
 
 for team in baseList['teams']:
     print("Fetching team " + str(team))
-    events = len(tba.team_events(int(team), YEAR, True, True))
-    f.write(str(team) + ", " + str(events) + "\n")
+    
+    eventCount = 0
+    
+    for event in tba.team_events(int(team), YEAR, True, False):
+        if event['event_type'] in normalEvents:
+            if event['key'] not in ignoreEvents:
+                eventCount += 1        
+    f.write(str(team) + ", " +str(eventCount) + "\n")
 
 f.close()
