9635e96c59fe7b64942a6e495c42a0d655c69ef6
Preston <childrep@vt.edu>
2018-05-17 23:22:17 -0400

    added wins and finalist reporting
    ryan why can't you count...

--- iriEvents.py
+++ iriEvents.py
@@ -14,17 +14,21 @@ normalEvents = [0,1,2,3,5]
 ignoreEvents = ['2018micmp', '2018oncmp']
 
 f = open("iriEvents" + str(YEAR) + ".csv", 'w', encoding='utf-8')
-f.write("team, events\n")
+f.write("team, events, wins, finalist\n")
 
 for team in baseList['teams']:
     print("Fetching team " + str(team))
     
     eventCount = 0
-    
+    winCount = 0
+    finalistCount = 0
+    for award in tba.team_awards(int(team), YEAR):
+        winCount += award['award_type'] == 1
+        finalistCount += award['award_type'] == 2
     for event in tba.team_events(int(team), YEAR, True, False):
         if event['event_type'] in normalEvents:
             if event['key'] not in ignoreEvents:
                 eventCount += 1        
-    f.write(str(team) + ", " +str(eventCount) + "\n")
+    f.write(str(team) + ", " + str(eventCount) + ", " + str(winCount) + ", " + str(finalistCount) + "\n")
 
 f.close()
