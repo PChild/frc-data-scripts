d0d7467ae26f356aa35fc74397952dd01395449d
Preston <childrep@vt.edu>
2018-05-26 21:52:43 -0400

    Updated general functions to have docs

--- gen.py
+++ gen.py
@@ -3,10 +3,28 @@ import tbapy
 import json
 import os
     
-def setup():
-    return tbapy.TBA(os.getenv("TBA_KEY"))
+def setup(apiKey="TBA_API", useEnv=True):
+    '''
+    Sets up tbapy with an API key from a system environment variable    
+    
+    :param str apiKey: The TBA API key or system environment variable name for it
+    :param bool useEnv: Set to True to fetch TBA API key from environment variable
+    
+    :return: Instance of TBA
+    '''
+    
+    if useEnv:
+        apiKey = os.getenv(apiKey)
+    
+    return tbapy.TBA(apiKey)
     
 def progressBar(value, endvalue, bar_length=20):
+        '''
+        Simple progress bar
+
+        :param value: The current progress level.
+        :param endvalue: The max for the progress bar.        
+        '''
 
         percent = float(value) / endvalue
         arrow = '-' * int(round(percent * bar_length)-1) + '>'
@@ -16,6 +34,13 @@ def progressBar(value, endvalue, bar_length=20):
         sys.stdout.flush()
         
 def listOfDictToCSV(filename, listObj):
+    '''
+    Saves a list of flat dictionaries out to a csv.
+    
+    :param filename: The file to write to
+    :param listObj: The list of objects to write out
+    '''
+    
     f = open(filename + "O.csv", 'w')
 
     for prop in listObj[0].keys():
@@ -29,5 +54,11 @@ def listOfDictToCSV(filename, listObj):
     f.close()
     
 def writeJsonFile(filename, dictionary):
+    '''
+    Writes a dictionary to a JSON file
+    
+    :param filename: The file to write to
+    :param dictionary: The dictionary to write out
+    '''
     with open(filename + '.json', 'w') as outfile:
         json.dump(dictionary, outfile)
