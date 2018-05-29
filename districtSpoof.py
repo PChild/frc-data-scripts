15300a733f4af0f94f3442b86a4fb0fdc19f3e80
Preston <childrep@vt.edu>
2018-05-29 13:50:18 -0400

    Testing file for emulating previous district rankings

---
+++ districtSpoof.py
@@ -0,0 +1,57 @@
+import gen
+
+tba = gen.setup()
+
+def getSpoofedDistrictData(district, year, spoofDate):
+    distString = str(year) + district
+    districtEvents = tba.district_events(distString, True)
+    districtPoints = tba.district_rankings(distString)
+    
+    completeEvents = []
+    fakeData = []
+
+    for team in districtPoints:
+        fakeData.append(dict(team))
+        fakeData[-1]['event_points'] = []
+        fakeData[-1]['point_total'] = 0
+
+    for event in districtEvents:
+        if event['end_date'] < spoofDate:
+            completeEvents.append(event['key'])
+    
+    for teamIdx, team in enumerate(districtPoints):
+        for event in team['event_points']:
+            if event['event_key'] in completeEvents:
+                fakeData[teamIdx]['event_points'].append(event)
+                fakeData[teamIdx]['point_total'] += event['total']
+
+    fakeData = sorted(fakeData, key = lambda k: k['point_total'], reverse=True)    
+    
+    for teamIdx, team in enumerate(fakeData):
+        team['rank'] = teamIdx + 1
+        
+    return processDistrictData(fakeData)
+
+def getDistrictData(district, year):
+    distString = str(year) + district
+    districtPoints = tba.district_rankings(distString)
+    
+    return processDistrictData(districtPoints)
+    
+
+def handleEvents(data):
+    events = []
+    
+    for event in data:
+        events.append(event['total'])
+    
+    return events
+        
+def processDistrictData(district):
+    distData = []
+    for team in district:
+        distData.append({'rank': team['rank'], 'total': team['point_total'], 'team': team['team_key'], 'events': handleEvents(team['event_points'])})
+    
+    return distData
+    
+dataTest = getSpoofedDistrictData('chs', 2018, '2018-03-17')
