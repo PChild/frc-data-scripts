0db7042f60b3e1e70320f5255f5ac32ea2ccd16e
Preston <childrep@vt.edu>
2018-05-31 23:40:41 -0400

    Fixed Ontario counting, added geolocating for MAR

--- districtGrowth.py
+++ districtGrowth.py
@@ -1,25 +1,32 @@
 import gen
+import geocoder
 
 tba = gen.setup()
 
 YEAR = 2018
 
-#Excluding MAR for now, I don't know how to handle partial inclusing of PA
-DISTRICTS = ['chs', 'fim', 'in', 'isr', 'ne', 'nc', 'ont', 'pch', 'pnw']
+DISTRICTS = ['chs', 'fim', 'in', 'isr', 'mar', 'ne', 'nc', 'ont', 'pch', 'pnw']
 
 DISTRICT_STATES = {'chs': ['Virginia', 'Maryland', 'District of Columbia', 'DC', 'VA, ''MD'],
                    'fim': ['Michigan', 'MI'],
                     'in': ['Indiana', 'IN'],
                     'isr': ['Israel'],
+                    'mar': ['New Jersey', 'Pennsylvania', 'Delaware', 'NJ', 'PA', 'DE'],
                     'ne': ['Massachusetts', 'Maine', 'Rhode Island', 'New Hampshire', 'Connecticut', 'Vermont', 'MA', 'ME', 'RI', 'NH', 'CT', 'VT'],
                     'nc': ['North Carolina', 'NC'],
-                    'ont': ['Ontario', 'ONT'],
+                    'ont': ['Ontario', 'ON', 'ONT'],
                     'pch': ['Georgia', 'GA'],
                     'pnw': ['Washington', 'Oregon', 'WA', 'OR']}
 
 
-districtStats = {}
+# Mid Atlantic Robotics Bylaws Article 1 Section 2 define the PA part of MAR as 'the counties of Pennsylvania including Harrisburg, eastward'
+# Harrisburg's longitude is listed as 76.884 , I used 77 as it's a round number and gives a ~7 mile margin.
+HARRISBURG_WEST_LON = -77
 
+districtStats = {}
+inMar = []
+outOfMar = []
+problemTeams = []
 
 for district in DISTRICTS:
     districtStats[district] = {}
@@ -37,14 +44,14 @@ for count, year in enumerate(yearRange):
         districtStats[district][str(year)]['teams'] = 0
     
     for district in currentDistricts:
-        if district['abbreviation'] not in ['mar', 'tx']:
+        if district['abbreviation'] != 'tx':
             districtStats[district['abbreviation']][str(year)]['dist'] = True
     
     
     teams = []
     for page in range(0,20):
         try:
-            newTeams = tba.teams(page, year, True)
+            newTeams = tba.teams(page, year)
             
             if newTeams == []:
                 break;
@@ -53,12 +60,44 @@ for count, year in enumerate(yearRange):
         except:
             pass
     for team in teams:
+        teamLng = -90
+        
         if team['country'] in DISTRICT_STATES['isr']:
             districtStats['isr'][str(year)]['teams'] += 1
-        
         for district in DISTRICTS:
             if team['state_prov'] in DISTRICT_STATES[district]:
-                districtStats[district][str(year)]['teams'] += 1
+                if team['state_prov'] in ['Pennsylvania', 'PA']:
+                    if team['team_number'] in inMar:
+                        districtStats[district][str(year)]['teams'] += 1
+                    elif team['team_number'] in outOfMar:
+                        pass
+                    else:
+                        try:
+                            if team['postal_code']:
+                                teamLng  = geocoder.osm(team['postal_code']).lng
+                            else:
+                                teamLng = geocoder.osm(team['city'] + ", " + team['state_prov']).lng
+                        except Exception as e:
+                            print(e)
+                            teamLng = -90
+                            problemTeams.append(team['team_number'])
+                            
+                            
+                        if teamLng:
+                            if teamLng > HARRISBURG_WEST_LON:
+                                inMar.append(team['team_number'])
+                                districtStats[district][str(year)]['teams'] += 1
+                            else:
+                                outOfMar.append(team['team_number'])
+                else:
+                    districtStats[district][str(year)]['teams'] += 1
+
+
+marTeams = tba.district_teams('2018mar')
+
+for team in marTeams:
+    if team['team_number'] in outOfMar:
+        print(team['team_number'], "is actually in MAR")
 
 districtTable = []
 for d in districtStats:
