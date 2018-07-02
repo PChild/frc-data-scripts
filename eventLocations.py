e6e8c18e344d8349c41f5cea2ba3f43fa6244ee4
Preston <childrep@vt.edu>
2018-07-02 16:32:05 -0400

    Updated to pull teams too, improved postal code handling.

--- eventLocations.py
+++ eventLocations.py
@@ -1,4 +1,5 @@
 import gen
+import geocoder
 from geoDicts import states, provs
 
 
@@ -6,10 +7,34 @@ tba = gen.setup()
 
 YEAR = 2018
 
-events = []
+data = []
 usedStates = []
 
-inDistricts = ['MI', 'IN', 'VA', 'MD', 'DC', 'NC', 'GA', 'RI', 'MA', 'ME', 'VT', 'WA', 'OR', 'DE', 'PA', 'NH', 'NJ', 'ON', 'TX']
+inDistricts = ['MI', 'IN', 'VA', 'MD', 'DC', 'NC', 'GA', 'RI', 'MA', 'ME', 'VT', 'WA', 'OR', 'DE', 'PA', 'NH', 'NJ', 'ON', 'TX', 'CT']
+
+newNames = []
+for abbrv in inDistricts:
+    try:
+        newNames.append(states[abbrv])
+    except:
+        newNames.append(provs[abbrv])
+inDistricts.append("Israel")
+inDistricts.append("ISR")
+inDistricts += newNames
+
+def handleZip(teamOrEvent):
+    try:
+        if teamOrEvent['postal_code'] == None:
+            tmpLoc = geocoder.osm(teamOrEvent['city'] + ", " + teamOrEvent['state_prov'] + " " + teamOrEvent['country'])
+            zipCode = geocoder.osm((tmpLoc.lat, tmpLoc.lng), method="reverse").postal
+        else:
+            zipCode = teamOrEvent['postal_code']
+        
+        if teamOrEvent['country'] == "Canada":
+            zipCode = zipCode[:3]
+    except:
+        zipCode = ""
+    return zipCode
 
 seasonEvents = tba.events(2018)
 for (idx, event) in enumerate(seasonEvents):
@@ -22,11 +47,13 @@ for (idx, event) in enumerate(seasonEvents):
     if event['state_prov'] == "TX" or event['state_prov'] == 'PA':
         district = True
     
-    if event['event_type'] in [0,1]:
-        zipCode = event['postal_code']
-        if event['country'] == "Canada":
-            zipCode = zipCode[:3]
-        events.append({"key": event['key'], "zip": zipCode, "state": event['state_prov'], "country": event['country'], "district": district})
+    if event['event_type'] in range(0,6):
+        data.append({"type": "Event", 
+                     "key": event['key'], 
+                     "zip": handleZip(event), 
+                     "state": event['state_prov'], 
+                     "country": event['country'], 
+                     "district": district })
         
         if event['state_prov'] not in usedStates:
             usedStates.append(event['state_prov'])
@@ -37,7 +64,12 @@ for state in states:
         
         if state in inDistricts:
             isDist = True
-        events.append({"key": "", "zip": "", "state": state, "country": "US", "district": isDist})
+        data.append({"type": "Event", 
+                     "key": "", 
+                     "zip": "", 
+                     "state": state, 
+                     "country": "US", 
+                     "district": isDist })
 
 for prov in provs:
     if prov not in usedStates:
@@ -45,7 +77,25 @@ for prov in provs:
         
         if prov in inDistricts:
             isDist = True    
-        events.append({"key": "", "zip": "", "state": prov, "country": "Canada", "district": isDist})  
+        data.append({"type": "Event",
+                     "key": "", 
+                     "zip": "", 
+                     "state": prov, 
+                     "country": "Canada", 
+                     "district": isDist })  
 
-colOrder = ["key", "zip", "state", "country", "district"]
-gen.listOfDictToCSV(str(YEAR) + "eventLocations", events, colOrder)
+
+teams = []
+
+for page in range(0,20):
+    gen.progressBar(page, 20)
+    for team in tba.teams(page, YEAR):
+        data.append({"type": "Team",
+                     "key": team['key'], 
+                     "zip": handleZip(team), 
+                     "state": team['state_prov'], 
+                     "country": team['country'], 
+                     "district": (team['country'] in inDistricts or team['state_prov'] in inDistricts) })  
+        
+colOrder = ["key", "type", "zip", "state", "country", "district"]
+gen.listOfDictToCSV(str(YEAR) + "eventLocations", data, colOrder)
