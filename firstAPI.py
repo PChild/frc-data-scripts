8f2a10167d30b58606a5afde5795ae830c37429a
Preston <childrep@vt.edu>
2018-08-30 14:30:07 -0400

    FIRST API querying works

--- firstAPI.py
+++ firstAPI.py
@@ -1,10 +1,17 @@
-import gen
+import os
 import base64
 import requests
 
+year = 2018
 user = 'CHILDREPT0401'
-token = gen.getFirstAPI()
-base = user + ':' + token
-authToken = base64.b64encode(base.encode())
+authKey = os.getenv('firstAPI')
+plainToken = user + ':' + authKey
+serverBase = 'https://frc-api.firstinspires.org/v2.0/' + str(year) + '/'
+authToken = base64.b64encode(plainToken.encode()).decode()
+headers = {'Authorization': 'Basic ' + authToken,
+           'Cache-Control': 'no-cache, max-age=10',
+           'Pragma': 'no-cache'}
 
-serverBase = 'https://frc-api.firstinspires.org'
+def getEventTeams(event):    
+    rawTeams = requests.get(serverBase + 'teams?eventCode=' + event, headers=headers).json()['teams']
+    return [team['teamNumber'] for team in rawTeams]
