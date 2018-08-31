2320b87f7b18d20a6b2bb727f605bd14fa10ba9b
Preston <childrep@vt.edu>
2018-08-31 11:11:54 -0400

    Better draft size data collection

--- draftSizes.py
+++ draftSizes.py
@@ -1,22 +1,36 @@
 import gen
+from tqdm import tqdm
 
 tba = gen.setup()
 
-teamCount = 20
-draftSize = teamCount * 3
+def getEventSizes(year=2018):
+    eventSizes = []
+    print('Getting event sizes')
+    for event in tqdm(tba.events(year)):
+        if event['event_type'] == 0:
+            eventSizes.append(len(tba.event_teams(event['key'], False, True)))
+    return eventSizes
 
-singleTier = 0
-multiTier = 0
-regionalCount = 0
-for event in tba.events(2018):
-    if event['event_type'] == 0:
-        regionalCount += 1
-        
-        teamCount = len(tba.event_teams(event['key'], False, True))
-        
-        if teamCount < draftSize:
-            multiTier += 1
-        else:
-            singleTier += 1
-        
-print("Of", regionalCount, "regionals,", multiTier, "would need multiple tiers.", )
+def saveRegionalData(year=2018):
+    eventData = []
+    print('Saving regional data')
+    for event in tqdm(tba.events(year)):
+        if event['event_type'] == 0:
+            eventData.append({'Key': event['key'], 'Size': len(tba.event_teams(event['key'], False, True))})
+    gen.listOfDictToCSV(str(year) + 'Regional Sizes', eventData)
+
+def savedDraftSizeData(eventSizes):
+    data = []    
+    for teamCount in range(1, 26):
+        draftSize = teamCount * 3
+        multiTier = 0
+        for event in eventSizes:
+            multiTier += event < draftSize
+        data.append({'SLFF Teams': teamCount, 'Multi-tier events': multiTier})
+    gen.listOfDictToCSV('Draft Sizing', data)
+    
+def main():
+    saveRegionalData()
+
+if __name__ == '__main__':
+    main()
