a2f02e80a57456648d31114c6bf4c23db6825d58
Preston <childrep@vt.edu>
2018-09-27 17:16:38 -0400

    Progress on ranksToPoints and regionCompare

--- rankToPoints.py
+++ rankToPoints.py
@@ -1,16 +1,38 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 import gen
-#import pandas
 
-tba = gen.setup()
+def generateData():
+    eventData = []
+    for val in range(1,25):
+        eventData.append([])
+    
+    for event in tba.events(2018):
+        if event['event_type'] == 0:
+            alliances = tba.event_alliances(event['key'])
+            distPoints = tba.event_district_points(event['key'])
+            
+            for allianceNum, alliance in enumerate(alliances):
+                for pickNum, team in enumerate(alliance['picks']):
+                    if pickNum == 0:
+                        selectionPos = (allianceNum + 1) * 2 - 1
+                    if pickNum == 1:
+                        selectionPos = (allianceNum + 1) * 2
+                    if pickNum == 2:
+                        selectionPos = 24 - allianceNum
+                    
+                    teamPts = distPoints['points'][team]
+                    eventData[selectionPos].append(teamPts['alliance_points'] + teamPts['elim_points'] + teamPts['qual_points'])
+    return eventData
 
-eventData = {}
+def readData():
+    print('memes')
 
-for event in tba.events(2018):
-    if event.event_type == 0:
-        alliances = tba.event_alliances(event.key)
-        
-        for idx, alliance in enumerate(alliances):
-            for team in alliance:
-                print('memes')
-        eventData[event] = []
-        
+def main():
+    #generateData()
+    #readData()
+    print('memes')
+
+if __name__ == '__main__':
+    tba = gen.setup()
+    main()
--- regionCompare.py
+++ regionCompare.py
@@ -7,22 +7,37 @@ regionTeams = {}
 
 usRegions = {'California': ['CA', 'California'],
            'Texas': ['TX', 'Texas', 'NM', 'New Mexico'],
-           'Midwest': ['OK', 'ND', 'SD', 'KS', 'WI', 'IL', 'NE', 'MO', 'IA'],
+           'Midwest': ['OK', 'Oklahoma', 'ND', 'North Dakota', 'SD', 'South Dakota',
+                       'KS', 'Kansas', 'WI', 'Wisconsin', 'IL', 'Illinois',
+                       'NE', 'Nebraska', 'MO', 'Missouri', 'IA', 'Iowa'],
            'Desert': ['NV', 'Nevada', 'AZ', 'Arizona'],
            'New York': ['NY', 'New York'],
-           'Mountain': ['CO', 'UT', 'ID', 'WY', 'MT'],
+           'Mountain': ['CO', 'Colorado', 'UT', 'Utah', 'ID', 'Idaho', 'WY', 'Wyoming', 'MT', 'Montana'],
            'South': ['Arkansas', 'LA', 'MS', 'AB'],
            'Florida': ['FL', 'Florida'],
            'Minnesota': ['MN', 'Minnesota'],
            'WOW': ['PA', 'WV', 'OH'],
-           'South Carolina': ['SC', 'South Carolina']}
+           'South Carolina': ['SC', 'South Carolina'],
+           'Hawaii': ['HI', 'Hawaii'],
+           'Quebec': ['Quebec', 'QC'],
+           'Alberta': ['Alberta', 'AB'],
+           'British Columbia': ['British Columbia', 'BC']}
 
-nonUsRegions = {}
+nonUsRegions = ['Australia', 'Turkey', 'Mexico', 'Brazil', 'China']
 
 districts = [item.key for item in tba.districts(YEAR)]
+distTeams = []
 for district in districts:
     if district != '2018tx':
-        regionTeams[district[4:]] = tba.district_teams(district, False, True)
+        districtTeams = tba.district_teams(district, False, True)
+        regionTeams[district[4:]] = districtTeams
+        distTeams += districtTeams
+        
+for region in usRegions:
+    regionTeams[region] = []
+
+for region in nonUsRegions:
+    regionTeams[region] = []
     
 teams = []
 for page in range(0,20):
@@ -31,3 +46,16 @@ for page in range(0,20):
             break;
         else:
             teams += newTeams
+            
+baseTeams = teams[:]
+for team in baseTeams:
+    if team.key in distTeams:
+        teams.remove(team)
+    for region in usRegions:
+        if team.state_prov in usRegions[region]:
+            regionTeams[region].append(team.key)
+            teams.remove(team)
+        if team.country in nonUsRegions:
+            regionTeams[team.country].append(team.key)
+            teams.remove(team)
+    
