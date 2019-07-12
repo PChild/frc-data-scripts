e2e69b3003d1d88aaecc7eb09a86efe888aa6466
Preston <childrep@vt.edu>
2019-07-11 22:00:33 -0400

    it werk

--- ras-effect.py
+++ ras-effect.py
@@ -1,15 +1,24 @@
+from tqdm import tqdm
 import tbapy
+import json
 import os
-
-year_data= {}
 
 if __name__ == '__main__':
     tba = tbapy.TBA(os.getenv('TBA_KEY'))
+    year_data = {}
 
-    for year in range(1992, 2020):
-        year_data[str(year)] = {'teams': [], 'gained': [], 'lost': []}
+    for year in tqdm(range(1992, 2020)):
+        s_year = str(year)
+        s1_year = str(year - 1)
+        year_data[s_year] = {'teams': tba.teams(year=year, keys=True), 'gains': [], 'losses': []}
 
-        if str(year - 1) in year_data:
-            year_data[str(year)]['gaind']
-        tba.teams(year=year, keys=True)
-    print('hi')
+        if s1_year in year_data:
+            year_data[s_year]['gains'] = [team for team in year_data[s_year]['teams']
+                                          if team not in year_data[s1_year]['teams']]
+            year_data[s_year]['losses'] = [team for team in year_data[s1_year]['teams']
+                                        if team not in year_data[s_year]['teams']]
+        else:
+            year_data[s_year]['gains'] = year_data[s_year]['teams']
+
+        with open('team_data_' + s_year + '.json', 'w') as fp:
+            json.dump(year_data[s_year], fp)
